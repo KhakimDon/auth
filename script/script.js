25ad2724d33682c57689a47f0682e582088568c4
khakimdon <almamedovkh@mail.ru>
2023-04-25 17:42:59 +0500

    html css

--- script/script.js
+++ script/script.js
@@ -1,15 +1,32 @@
-let button = document.querySelector(".post")
-button.addEventListener("click", () => post())
+// let button = document.querySelector(".post")
+// button.addEventListener("click", () => post())
 
 
 
-let api = "https://6411c0685d0c49b7c5b8fb8c.mockapi.io/yanki/market"
+// let api = "https://6411c0685d0c49b7c5b8fb8c.mockapi.io/yanki/market"
 
-function post(){
-    axios.post(api,{
-        title: "hello",
-        password: 1234,
-    })
+// function post(){
+//     axios.post(api,{
+//         title: "hello",
+//         password: 1234,
+//     })
+// }
+
+
+
+
+let login = document.forms.login
+let sign = document.forms.sign
+
+let reg = document.querySelector("#reg")
+let log = document.querySelector("#log")
+
+reg.onclick = () =>{
+    login.classList.add("none")
+    sign.classList.remove("none")
 }
-
+log.onclick = () =>{
+    login.classList.remove("none")
+    sign.classList.add("none")
+}
 
